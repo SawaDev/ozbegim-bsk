2aa8b98023c4ba3192b4841a50b275070d403e67
sardormahmudovbellissimo <sardor.mahmudov@bellissimo.uz>
2025-09-18 14:02:49 +0500

    data changed

--- src/data.ts
+++ src/data.ts
@@ -4,29 +4,93 @@ export const branches = [
     contacts: [
       {
         title: "Santexnik",
-        name: "Sanjar Abdullayev",
-        phone: "+998991111111",
-        work_time: "9:00 - 18:00"
+        employees: [
+          {
+            name: "Matnazarov Xemra",
+            phone: "+998881119094",
+            work_time: "9:00 - 18:00"
+          }
+        ]
+      },
+      {
+        title: "Elektrik",
+        employees: [
+          {
+            name: "Yuldashev Jalg'azboy",
+            phone: "+998942350080",
+            work_time: "9:00 - 18:00"
+          }
+        ]
+      },
+      {
+        title: "Yordamchi ishchi",
+        employees: [
+          {
+            name: "Mirxoliqov O'rinboy",
+            phone: "+998944166954",
+            work_time: "9:00 - 18:00"
+          }
+        ]
       },
       {
         title: "Bog'bon",
-        name: "Abdulla Abdullayev",
-        phone: "+998992222222"
+        employees: [
+          {
+            name: "Omono'v To'ychiboy",
+            phone: "+998944171380",
+            work_time: "9:00 - 18:00"
+          }
+        ]
       },
       {
-        title: "qanaqadur ish",
-        name: "Abdulla Abdullayev",
-        phone: "+998992222222"
-      }
-    ]
-  },
-  {
-    name: "Shoshkent",
-    contacts: [
+        title: "Farrosh",
+        employees: [
+          {
+            name: "Ergashova Shahnoza",
+            phone: "+998938560286",
+            work_time: "8:00 - 17:00"
+          },
+          {
+            name: "Utamuratova Shirin",
+            phone: "+998973535678",
+            work_time: "8:00 - 17:00"
+          },
+          {
+            name: "Toirova Nodira",
+            phone: "+998931568446",
+            work_time: "8:00 - 17:00"
+          }
+        ]
+      },
       {
-        title: "Santexnik",
-        name: "Sardor Abdullayev",
-        phone: "+998993333333"
+        title: "Oxrana",
+        employees: [
+          {
+            name: "Boymatov Sardor",
+            phone: "+998991170794",
+            work_time: "24 soat"
+          },
+          {
+            name: "To'laganxo'jayev Nodirxo'ja",
+            phone: "+998331170701",
+            work_time: "24 soat"
+          },
+          {
+            name: "Eshov Bahodir",
+            phone: "+998939198189",
+            work_time: "24 soat"
+          },
+          {
+            name: "To'xtayev Fazliddin",
+            phone: "+998881771009",
+            work_time: "24 soat"
+          },
+          {
+            name: "Shakarbekov Azamat",
+            phone: "+998994688940",
+            work_time: "24 soat"
+          }
+        ]
       }
     ]
   },
@@ -34,9 +98,74 @@ export const branches = [
     name: "Chaqar",
     contacts: [
       {
+        title: "Santexnik",
+        employees: [
+          {
+            name: "Matnazarov Xemra",
+            phone: "+998881119094",
+            work_time: "9:00 - 18:00"
+          }
+        ]
+      },
+      {
+        title: "Elektrik",
+        employees: [
+          {
+            name: "Yuldashev Jalgazboy",
+            phone: "+998942350080",
+            work_time: "9:00 - 18:00"
+          }
+        ]
+      },
+      {
         title: "Bog'bon",
-        name: "Alisher Abdullayev",
-        phone: "+998995555555"
+        employees: [
+          {
+            name: "Ortiqov Begzod",
+            phone: "+998998218056",
+            work_time: "9:00 - 18:00"
+          }
+        ]
+      },
+      {
+        title: "Farrosh",
+        employees: [
+          {
+            name: "Alimova Ziyoda",
+            phone: "+998888781450",
+            work_time: "8:00 - 17:00"
+          },
+          {
+            name: "Raimjonova Munisa",
+            phone: "+998933060208",
+            work_time: "8:00 - 17:00"
+          },
+          {
+            name: "Abduraxmonova Gulnoza",
+            phone: "+998931168774",
+            work_time: "8:00 - 17:00"
+          }
+        ]
+      },
+      {
+        title: "UK Ish bosqaruvchisi",
+        employees: [
+          {
+            name: "Tinchlikov Sirojiddin",
+            phone: "+998885171795",
+            work_time: "9:00 - 18:00 (shanba kunlari: 9:00 - 15:00)"
+          }
+        ]
+      },
+      {
+        title: "UK kasser",
+        employees: [
+          {
+            name: "Abduvaliyeva Mohinur",
+            phone: "+998900965350",
+            work_time: "9:00 - 18:00 (shanba kunlari: 9:00 - 15:00)"
+          }
+        ]
       }
     ]
   }
